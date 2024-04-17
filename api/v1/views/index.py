7d3e828d1c877f8d002340e391954a84ad959e16
mistWil <7448@holbertonstudents.com>
2024-04-17 12:16:19 +0200

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,6 +8,7 @@ Index view
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -16,3 +17,19 @@ def status():
     Return JSON status response
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    Return JSON with count of objects by type
+    """
+    classes = {
+        "Amenity": storage.count("Amenity"),
+        "City": storage.count("City"),
+        "Place": storage.count("Place"),
+        "Review": storage.count("Review"),
+        "State": storage.count("State"),
+        "User": storage.count("User")
+    }
+    return jsonify(classes)
